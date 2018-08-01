c056cf44ed9bd40cb61030aae27bed8033a807ea
吴鑫康 <zjxjwxk@gmail.com>
2018-08-01 20:32:10 +0800

    Add AVLMap

--- Map/src/Main.java
+++ Map/src/Main.java
@@ -41,5 +41,11 @@ public class Main {
         LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
         double time2 = testMap(linkedListMap, filename);
         System.out.println("Linked List Map: " + time2 + " s");
+
+        System.out.println();
+
+        AVLMap<String, Integer> avlMap = new AVLMap<>();
+        double time3 = testMap(avlMap, filename);
+        System.out.println("AVL Map: " + time3 + " s");
     }
 }
