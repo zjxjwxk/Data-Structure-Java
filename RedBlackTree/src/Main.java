3c88224a84dc9fafe8a456addcdd395b01a3522a
吴鑫康 <zjxjwxk@gmail.com>
2018-08-02 17:43:51 +0800

    RedBlackTree

---
+++ RedBlackTree/src/Main.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+
+            // Collections.sort(words);
+
+            // Test BST
+            long startTIme = System.nanoTime();
+
+            BST<String, Integer> bst = new BST<>();
+            for (String word :
+                    words) {
+                if (bst.contains(word)) {
+                    bst.set(word, bst.get(word) + 1);
+                } else {
+                    bst.add(word, 1);
+                }
+            }
+
+            for (String word :
+                    words) {
+                bst.contains(word);
+            }
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTIme) / 1000000000.0;
+            System.out.println("BST: " + time + " s");
+
+            // Test AVL Tree
+            startTIme = System.nanoTime();
+
+            AVLTree<String, Integer> avl = new AVLTree<>();
+            for (String word :
+                    words) {
+                if (avl.contains(word)) {
+                    avl.set(word, avl.get(word) + 1);
+                } else {
+                    avl.add(word, 1);
+                }
+            }
+
+            for (String word :
+                    words) {
+                avl.contains(word);
+            }
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTIme) / 1000000000.0;
+            System.out.println("AVL: " + time + " s");
+
+            // Test RBTree
+            startTIme = System.nanoTime();
+
+            RBTree<String, Integer> brt = new RBTree<>();
+            for (String word :
+                    words) {
+                if (brt.contains(word)) {
+                    brt.set(word, brt.get(word) + 1);
+                } else {
+                    brt.add(word, 1);
+                }
+            }
+
+            for (String word :
+                    words) {
+                brt.contains(word);
+            }
+
+            endTime = System.nanoTime();
+
+            time = (endTime - startTIme) / 1000000000.0;
+            System.out.println("RBTree: " + time + " s");
+        }
+    }
+}
---
+++ RedBlackTree/src/Main3.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        int n = 20000000;
+
+        ArrayList<Integer> testData = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            testData.add(i);
+        }
+
+        // Test BST
+//        long startTime = System.nanoTime();
+//
+//        BST<Integer, Integer> bst = new BST<>();
+//        for (Integer x :
+//                testData) {
+//            bst.add(x, null);
+//        }
+//
+//        long endTime = System.nanoTime();
+//
+//        double time = (endTime - startTime) / 1000000000.0;
+//        System.out.println("BST: " + time + " s");
+
+        // Test BST
+        long startTime = System.nanoTime();
+
+        AVLTree<Integer, Integer> avl = new AVLTree<>();
+        for (Integer x :
+                testData) {
+            avl.add(x, null);
+        }
+
+        long endTime = System.nanoTime();
+
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println("AVL: " + time + " s");
+
+        // Test BST
+        startTime = System.nanoTime();
+
+        RBTree<Integer, Integer> rbt = new RBTree<>();
+        for (Integer x :
+                testData) {
+            rbt.add(x, null);
+        }
+
+        endTime = System.nanoTime();
+
+        time = (endTime - startTime) / 1000000000.0;
+        System.out.println("RBTree: " + time + " s");
+    }
+}
